7d95421a61433675fe5869ef2d9a874d43cd6bc9
phaniram <maturusrinivasphaniram@gmail.com>
2014-01-24 03:45:34 -0500

    single process including web

--- scraper.js
+++ scraper.js
@@ -1,3 +1,4 @@
+var http = require('http');
 (function () {
   "use strict";
     var jsdom = require('jsdom');
@@ -145,3 +146,8 @@
   if (interval > 0) setInterval(scraper, interval);
 
 }) ();
+var port = Number(process.env.PORT || 5000);
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello User \n');
+}).listen(port);
