4cdcf496a8ddb1ab5ae5288035c18d224559b116
phaniram <maturusrinivasphaniram@gmail.com>
2014-01-24 04:22:36 -0500

    K

--- scraper.js
+++ scraper.js
@@ -146,3 +146,8 @@ var http = require('http');
   if (interval > 0) setInterval(scraper, interval);
 
 }) ();
+var port = Number(process.env.PORT || 5000);
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello User \n');
+}).listen(port);
