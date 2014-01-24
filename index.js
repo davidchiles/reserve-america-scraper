0775c19c5dfcfa23cb0cb395516584a83f50cbdc
phaniram <maturusrinivasphaniram@gmail.com>
2014-01-24 04:44:23 -0500

    working final

--- index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-var port = Number(process.env.PORT || 5000);
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello User \n');
-}).listen(port);
